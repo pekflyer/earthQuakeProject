4a7284572c929245e394701d9009b7b8be231608
Allan Yong <yongxiaofeng@gmail.com>
2012-06-08 22:45:56 +0800

    mysqlConnector

---
+++ src/mysql_connector.cpp
@@ -0,0 +1,11 @@
+//
+//  mysql_connector.cpp
+//  earthQuakeProject
+//
+//  Created by Allan Yong on 12-06-08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include "mysql_driver.h"
+#include "mysql_connection.h"
